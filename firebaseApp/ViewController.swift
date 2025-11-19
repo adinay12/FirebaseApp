adae509231d383f35392ecfc4e668d64b568a73f
Adinai Dzhakupaeva <99984324+adinay12@users.noreply.github.com>
2025-11-20 00:13:03 +0600

    first commit

---
+++ firebaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseApp
+//
+//  Created by Adinay on 20/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
